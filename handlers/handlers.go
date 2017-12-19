8afdf6fe3b13df7b1c6c90fe7e6d52fd36a816e8
Alex Ellis <alexellis2@gmail.com>
2017-12-19 20:56:11 +0000

    Initial port from faas to faas-swarm
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ handlers/handlers.go
@@ -0,0 +1,17 @@
+package handlers
+
+// import (
+// 	"net/http"
+// )
+
+// func ReplicaReader() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+
+// 	}
+// }
+
+// func ReplicaUpdater() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+
+// 	}
+// }
